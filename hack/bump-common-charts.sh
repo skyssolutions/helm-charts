503d08109da52c65d6e625cdf586a1c8e8d14639
Skyler Mäntysaari <sm+git@skym.fi>
2022-08-21 08:02:18 +0300

    Remove everything except index.yaml.

--- hack/bump-common-charts.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-find charts/stable/ -name "Chart.yaml" -exec grep -q -l "\- name\: common" {} \; -print0 | xargs -0 sed -i '' -r -e "s|^version: ([0-9]+\.[0-9]+)\.[0-9]+$|version: \1.1|"
